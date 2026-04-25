349dd42f0db92afeb6cdffab6b5a0c64bda69882
Rodolfo Oliveira <rodolfo.oli29@gmail.com>
2026-04-24 22:38:39 -0300

    docs: configura Swagger para documentação da API

--- src/main/java/com/controle/ControleOrcamentaisApplication.java
+++ src/main/java/com/controle/ControleOrcamentaisApplication.java
@@ -3,7 +3,17 @@ package com.controle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 @SpringBootApplication
+@OpenAPIDefinition(
+    info = @Info(
+        title = "API de Gestão de Ordens de Serviço",
+        version = "1.0",
+        description = "Sistema desenvolvido para eliminar o uso de papel na oficina, gerenciando orçamentos e funcionários."
+    )
+)
+//@SpringBootApplication
 public class ControleOrcamentaisApplication {
 
 	public static void main(String[] args) {
